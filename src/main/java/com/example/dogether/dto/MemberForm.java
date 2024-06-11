4c035daf47deeb7b672f6e66a36f796dde506d77
jeeseula <jeeseula@gmail.com>
2024-06-11 14:25:36 +0900

    회원가입 전화번호 form 디버깅

--- src/main/java/com/example/dogether/dto/MemberForm.java
+++ src/main/java/com/example/dogether/dto/MemberForm.java
@@ -29,6 +29,7 @@ public class MemberForm {
     @NotEmpty(message = "이름은 필수입니다")
     private String name;
 
+    @NotEmpty(message = "전화번호는 필수입니다")
     private String tel;
 
     private String address;
